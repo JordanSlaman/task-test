3b1fe9369745c87a4107463697cc86383697fa75
Jordan <jordan.slaman@gmail.com>
2016-08-10 16:09:10 -0700

    Fixed the Task object id, so that it is unique for each new task. Added a remove_task method that removes one task by id, and associated helper functions.

--- Python Version/main.py
+++ Python Version/main.py
@@ -1,47 +1,81 @@
 """ Todo:
-        - Add a remove_task method that removes only one task by id
-
         - Upon calling complete() on a task, set _value of that task object to the number of occurrences of the
             string "CCN" (case in-sensitive) that appears in the task's name.
 
-        - Fix the Task object id, so that it is unique for each new task. (please consider scalability and
-            what else the Id could be used for)
-
         - Fix other bugs and make improvements where you see fit
         - Add error handling where you see fit
-
-    Note:
-        - You cannot edit/change the TaskManager class directly. Think of it as a 3rd party library
-        - You can create new objects, etc
 """
 
+import hashlib, datetime
 from task_manager import  Task, TaskManager
 
 class CustomTaskManager(TaskManager):
 
+    def __init__(self):
+        self._tasks = dict()
+
+    def generate_key(self,task):
+        m = hashlib.md5()
+        m.update(task.name + task.created)
+        return m.hexdigest()
+
+    def import_task(self,task):
+        self._tasks[self.generate_key(task)] = task
+
     def complete_tasks(self):
-        if len(self._tasks) > 0:
-            if not all([task.is_completed for task in self._tasks]):
-                for task in self._tasks:
-                    if not task.is_completed:
-                        task.complete()
-                        print('task {name} completed'.format(name=task.name))
+        for task in self._tasks.items():
+            if not task[1].is_completed:
+                task[1].complete()
+                print('task {name} completed'.format(name=task[1].name))
+
+    def remove_task(self,key):
+        del self._tasks[key]
+
+    def remove_tasks(self):
+        self._tasks.clear()
+
+    def lookup_tasks(self,name):
+        results_subset = dict()
+        for task in self._tasks.items():
+            if task[1].name is name:
+                results_subset[task[0]] = task[1]
+        return results_subset
+
+    def killall(self, name):
+        for t in self.lookup_tasks(name).items():
+            self.remove_task(t[0])
+
+class CustomTask(Task):
+
+    def __init__(self, name):
+        self._created = unicode(datetime.datetime.now())
+        self._name = name
+        self._value = None
+        self._completed = False
+
+    def complete(self):
+        _completed = True
+
+    @property
+    def created(self):
+        return self._created
 
 
 if __name__ == "__main__":
-    task_manager = TaskManager()
-    first_task = Task('!!nZ@xr>492CCN;SDRC2#6CcN_$5UcCNq]*m44AhW`')
-    second_task = Task('g}~x?C*n9K|LccN_YEL@<=44jkc.dB-v{!#;7*[[')
-    third_task = Task('ekCcN,h9=!B46)j6acCN;`n68M+2ZR2CCn^:CUw')
+    task_manager = CustomTaskManager()
 
-    task_manager.import_task(first_task)
-    task_manager.import_task(second_task)
-    task_manager.import_task(third_task)
+    new_tasks = ['!!nZ@xr>492CCN;SDRC2#6CcN_$5UcCNq]*m44AhW`',
+                'g}~x?C*n9K|LccN_YEL@<=44jkc.dB-v{!#;7*[[',
+                'ekCcN,h9=!B46)j6acCN;`n68M+2ZR2CCn^:CUw']
+    for task in new_tasks:
+        task_manager.import_task(CustomTask(task))
 
     task_manager.complete_tasks()
 
-    last_task = Task('>.`8tCcn{xsS3sa!G@{cCn(w},U+s)**sACc]NAn#')
-    task_manager.import_task(last_task)
+    #invokes the remove_task method by name as opposed to it's key.
+    task_manager.killall('g}~x?C*n9K|LccN_YEL@<=44jkc.dB-v{!#;7*[[')
+
+    task_manager.import_task(CustomTask('>.`8tCcn{xsS3sa!G@{cCn(w},U+s)**sACc]NAn#'))
     task_manager.complete_tasks()
 
     task_manager.remove_tasks()
--- README.md
+++ README.md
@@ -16,3 +16,20 @@ We are looking forward to see your solution.
 # Jordan's Solution
 
 I have chosen to work on the Python implementation.
+
+## ID's
+I've decided that it would be best to store the tasks as a dict, as it would make it much easier to track reference them by ID than by array position. In this implementation a task would not need to know it's ID, which I feel believe makes for better encapsulation - Unfortunately it results in overwriting all of the methods in the original Task Manager.
+
+It's possible that multiple tasks would be created with the same name, so in order to avoid collisions I assigned the keys by hashing both the name and the timestamp of creation concatenated. Theoretically there could still be collisions, but that can be fixed/optimized with some error checking code, a larger hash digest and/or a finer time granularity.
+
+Scheduling/Ordering of tasks was not addressed, but that could be done by prioritizing the created timestamp, or inserting them in a different processing queue as they are allocated/preformed.
+
+### Finding Keys
+The lookup\_tasks(name) function returns a subset of the tasks dict with matching names, alternatively if you are iterating over/operating on a task somewhere you could re-compute it with generate\_key(task).
+
+### Scaling
+The task manager could be theoretically be scaled to thousands of application servers operating on tasks stored in a redis cluster for high availibility. Performance bottlenecks could be addressed as they appear.
+
+I haven't done comparisons of the algorithmic complexity of my changes.
+
+I chose not to modify the 'task_manager.py' file at all in order to respect the 'external library' concept - so all the original bugs are included.
